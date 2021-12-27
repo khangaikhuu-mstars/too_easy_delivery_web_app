3f75c6bdbcd1f25290046435570d1c2fb3555702
Mstars_Lab1_13 <mstars_lab1_13@Mstars-Lab1-13s-iMac.local>
2021-12-27 12:50:40 +0800

    #5 [ADD] Footer.js

---
+++ project/src/components/Footer.js
@@ -0,0 +1,27 @@
+
+
+function Footer(){
+    return(
+    
+        <footer>
+            <h4>Food Delivery</h4>
+            <div className="row">
+                <div className="col">
+                    <p><a>Нүүр</a></p>
+                    <p><a>Хоолны цэс</a></p>
+                    <p><a>Хүргэлтийн бүс</a></p>
+                </div>
+                <div className="col">
+                    <p> Холбоо барих
+                    </p>
+                    <p> (976) 77123456</p>
+                    <p> (976) 77123345</p>
+                </div>
+            </div>
+        </footer>
+
+    
+    )
+}
+
+export default Footer;
