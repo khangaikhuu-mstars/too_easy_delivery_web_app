fca787ae103236329b6b1ebf3ebf9b0ed66af16a
lab_1_1x <lab_1_15@lab-1-15s-iMac.local>
2021-12-28 12:51:51 +0800

    #2

--- project/src/components/menu.js
+++ project/src/components/menu.js
@@ -4,40 +4,25 @@ import foodDeliveryLogo from '../image/fooddelivery.svg'
 import searchLogo from '../image/search.svg'
 import adminLogo from '../image/admin.svg'
 import button from '../image/button.svg'
-import basketIcon from '../image/basketIcon.svg'
-import userIcon from '../image/userIcon.png'
 function Menu() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light">
-            <div class="container">
-                <div class="d-flex first-div">
-                    <img src={foodDeliveryLogo} alt=""  className="deliveryLogo"/>
-                    <h1 class="foodDelivery">Food Delivery</h1>
+        <Navbar  expand={false}>
+            <Container>
+                <div>
+                    <img src={button} className='button'></img>
                 </div>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-link" aria-current="page" href="#" class='orangeFont'>Нүүр</a>
-                        <a class="nav-link" href="#">Хоолны цэс</a>
-                        <a class="nav-link" href="#">Хүргэлтийн бүс</a>
-                    </div>
+                <div className='d-flex mt-2'>
+                    <div className='foodDeliveryLogo'></div>
+                    <div><h1 className='foodDelivery mt-1'>Food Delivery</h1></div>
                 </div>
                 <div>
-                    <input class="form-control me-2" type="search" placeholder="Хайх" aria-label="Search"></input>
+                   <img src={searchLogo} className='searchLogo'></img>
                 </div>
                 <div>
-                    <img src={basketIcon}></img>
-                    <p1 className='orangeFont'>Сагс</p1>
+                    <img src={adminLogo} className='adminLogo'></img>
                 </div>
-                <div>
-                    <img src={userIcon}></img>
-                    <p1 className='orangeFont'>Нэвтрэх</p1>
-                </div>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-            </div>
-        </nav>
+            </Container>
+        </Navbar>
     )
 }
 
