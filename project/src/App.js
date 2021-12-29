03868280df0cd9c4382807c5cd8f53a593692391
Mstars_Lab1_13 <mstars_lab1_13@Mstars-Lab1-13s-iMac.local>
2021-12-29 14:12:00 +0800

    #5[EDIT] format fixes

--- project/src/App.js
+++ project/src/App.js
@@ -1,18 +1,16 @@
-
 import './css/footer.css'
 import Footer from './components/Footer';
 import './App.css';
 import React from 'react';
 import InfoPanel from './components/InfoPanel';
-
 import Slider from './components/Slider';
 import '../src/css/Info_panel.css'
 
 function App() {
   return (
     <div>
-      {/* <Slider/> */}
-      {/* <InfoPanel/> */}
+      <Slider/>
+      <InfoPanel/>
       <Footer />
     </div>
     )
