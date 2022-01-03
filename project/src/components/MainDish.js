36b72c47765050fbb64434bc49289373cf3573ea
lab_1_19 <mstarsacademy@MStarss-iMac.local>
2022-01-03 12:48:01 +0800

    #35 Created MainDish Compnonent

---
+++ project/src/components/MainDish.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { ButtonComponent } from "./ButtonComponent";
+function MainDish() {
+    return (
+        <div>
+            <h2> Үндсэн хоол</h2>
+            <div><ButtonComponent buttonStyle='btn--withArrow' buttonSize='' ></ButtonComponent></div>
+        </div>
+    )
+}
+export default MainDish;
