5ad8bea6e882af0d3aa85e8bc2afd3bceb1d9e6b
lab_1_19 <mstarsacademy@MStarss-iMac.local>
2022-01-03 17:52:43 +0800

    #35 edited

--- project/src/components/CardComp.js
+++ project/src/components/CardComp.js
@@ -14,13 +14,11 @@ const CardComp = () => {
                 <Card.Img variant="top" src={foodsDetail.thumb_img} />
                 <Card.Title>{foodsDetail.name}</Card.Title>
                 <Card.Text style={{ color: '#f17228' }}>{foodsDetail.price}</Card.Text>
-                <Card.Text>{foodsDetail.price}</Card.Text>
-                <Card.Text>{foodsDetail.price}</Card.Text>
-                <Card.Text>{foodsDetail.price}</Card.Text>
-                <Card.Text>{foodsDetail.price}</Card.Text>
-                <Card.Text>{foodsDetail.price}</Card.Text>
-                <Card.Text>{foodsDetail.price}</Card.Text>
-                <Card.Text>{foodsDetail.price}</Card.Text>
+                <Card.Text style={{display: 'none'}} >{foodsDetail.sales}</Card.Text>
+                <Card.Text style={{display: 'none'}} >{foodsDetail.portion}</Card.Text>
+                <Card.Text style={{display: 'none'}} >{foodsDetail.stock}</Card.Text>
+                <Card.Text style={{display: 'none'}} >{foodsDetail.recipe}</Card.Text>
+                <Card.Text style={{display: 'none'}} >{foodsDetail.category}</Card.Text>
               </Card.Body>
             </Card>
           </div>
