dfd05956587af6ebbd44b4f35745878dc1d062c0
lab_1_19 <mstarsacademy@MStarss-iMac.local>
2022-01-04 14:14:49 +0800

    #36 fixed merge complict

--- project/src/App.js
+++ project/src/App.js
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
 import React from 'react';
 import Footer from './components/Footer';
 import Menu from './components/menu';
-=======
-import React from 'react'
-import Footer from './components/Footer'
-import Menu from './components/menu'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Delivery from './components/delivery'
 import Main from './components/main'
 import Home from './components/home'
->>>>>>> 9ddf8d34e8103314b27d79f5f2e9ea36f4991491
 import './css/footer.css'
 import './App.css'
 import '../src/css/InfoPanel.css'
