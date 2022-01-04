d8663f811795f1af29854a182b45ff4342c21b98
lab_1_01 <lab_1_01@lab-1-01s-iMac.local>
2022-01-04 14:16:41 +0800

    #13 slider end

--- project/src/App.js
+++ project/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Footer from './components/Footer';
-import SliderWeb from './components/sliderWeb';
+import SliderWeb from './components/SliderWeb';
 import InfoPanel from './components/InfoPanel';
 import Menu from './components/menu';
 import MainDish from './components/MainDish';
