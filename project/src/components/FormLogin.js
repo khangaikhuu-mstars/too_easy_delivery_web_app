320f471241785433a2cc20857b78bd0124b5d359
Temka1358 <temulenn@gmail.com>
2022-01-04 22:48:07 +0800

    login form mobile is done

--- project/src/components/FormLogin.js
+++ project/src/components/FormLogin.js
@@ -4,30 +4,34 @@
 
 function FormLogin(){
 return (
-    <>  
-        <div className="container d-flex login-form-header text-center d-block d-md-none">
-            <div className="">&#60;</div>
+    <div className="login-form">  
+        <div className="form-mobile-header my-3 py-2 d-flex login-form-header text-center d-block d-md-none">
+            <div className="ps-2">&#60;</div>
             <div className="flex-grow-1 align-self-center text-align-center">НЭВТРЭХ</div>
         </div>
         <form>
-            <div className="container">
+            <div className="container px-0">
                 <div className="form-email">
                     <label for="exampleInputEmail1" className="form-label">И-мэйл</label>
                     <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="И-мэйл хаягаа оруулна уу."/>
                 </div>
-                <div className="form-password mt-4">
+                <div className="form-password mt-3">
                     <label for="exampleInputPassword1" className="form-label">Нууц үг</label>
                     <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Нууц үгээ оруулна уу."/>
-                    <p className="align">Нууц үгээ марсан уу?</p>
                 </div>
-                    <button type="submit" className="btn">НЭВТРЭХ</button>
-                    <p>эсвэл</p>
-                <div>
-                    <a>БҮРТГҮҮЛЭХ</a>
+                <div className="forget-password text-end">
+                    <p className="">Нууц үгээ марсан уу?</p>
                 </div>
+                <button type="submit" className="btn mt-5" id="login-button">НЭВТРЭХ</button>
             </div>
         </form>
-    </>
+        <div className="container px-0">
+            <p className="text-decoration text-center my-3 text-muted"><u>эсвэл</u></p>
+            <div className="text-center py-1" id="register-link">
+                    <a>БҮРТГҮҮЛЭХ</a>
+            </div>
+        </div>
+    </div>
 )
 };
 
