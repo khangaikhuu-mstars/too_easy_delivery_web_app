d56da9942d350361fbabdba9b51e00f02b19b2b6
lab_1_1x <lab_1_15@lab-1-15s-iMac.local>
2022-01-04 09:49:14 +0800

    latest menu

--- project/src/App.js
+++ project/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Footer from './components/Footer';
 import SliderWeb from './components/SliderWeb';
 import InfoPanel from './components/InfoPanel';
-import CardComp from "./components/CardComp";
 import Menu from './components/menu';
 import MainDish from './components/MainDish';
 import './css/footer.css'
@@ -10,13 +9,13 @@ import './App.css';
 import '../src/css/InfoPanel.css'
 import '../src/css/sliderWeb.css'
 import '../src/css/menu.css'
-import { BrowserRouter } from 'react-router-dom';
+
 function App() {
   return (
     <div>
       <Menu />
       <SliderWeb />
-      <InfoPanel></InfoPanel>
+      <InfoPanel />
       <MainDish/>
       <Footer />
     </div>
