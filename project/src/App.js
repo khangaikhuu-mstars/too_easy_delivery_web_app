fbd9b9ccbeed5426fac0e4bc1af88321c2036e53
khangaikhuu uvgunkhuu <khangaikhuu@mstars.mn>
2022-01-04 13:54:27 +0800

    [ADD] remove unnecessary imports

--- project/src/App.js
+++ project/src/App.js
@@ -1,29 +1,25 @@
-import React from 'react';
-import Footer from './components/Footer';
-import SliderWeb from './components/SliderWeb';
-import InfoPanel from './components/InfoPanel';
-import Menu from './components/menu';
-import MainDish from './components/MainDish';
-import SaladAndDesert from './components/SaladAndDesert';
+import React from 'react'
+import Footer from './components/Footer'
+import Menu from './components/menu'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Delivery from './components/delivery'
+import Main from './components/main'
+import Home from './components/home'
 import './css/footer.css'
-import './App.css';
+import './App.css'
 import '../src/css/InfoPanel.css'
 import '../src/css/sliderWeb.css'
 import '../src/css/menu.css'
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Delivery from './components/delivery';
-import Main from './components/main'
-import Home from './components/home';
+
 function App() {
   return (
-
     <BrowserRouter>
       <div>
         <Menu />
         <Switch>
-          <Route path='/home' component={Home}></Route>
-          <Route path='/delivery' component={Delivery}></Route>
-          <Route path='/main' component={Main}></Route>
+          <Route path="/home" component={Home}></Route>
+          <Route path="/delivery" component={Delivery}></Route>
+          <Route path="/main" component={Main}></Route>
         </Switch>
         <Footer />
       </div>
@@ -31,4 +27,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
