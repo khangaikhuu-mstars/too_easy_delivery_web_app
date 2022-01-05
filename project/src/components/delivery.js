19be83e861864b1cf55d70ede5846c101f88d57a
lab_1_01 <lab_1_01@lab-1-01s-iMac.local>
2022-01-05 15:43:07 +0800

    #52 issue map

--- project/src/components/delivery.js
+++ project/src/components/delivery.js
@@ -1,7 +1,12 @@
 import React from "react";
-function Delivery(){
-    return(
-        <h1>Delivery</h1>
+import DeliveryRadius from "./DeliveryRadius";
+function Delivery() {
+    return (
+        <div>
+            <DeliveryRadius />
+        </div>
+
+
     )
 }
 export default Delivery
--- project/src/index.js
+++ project/src/index.js
@@ -6,7 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/menu.css'
 import './css/sliderWeb.css';
-import './css/mainDish.css'
+import './css/mainDish.css';
+import './css/DeliveryRadius.css'
 
 ReactDOM.render(
   <React.StrictMode>
