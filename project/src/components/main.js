b1a97c23916ab15e4483a19a3e2bc5f3f0033661
lab_1_1x <lab_1_15@lab-1-15s-iMac.local>
2022-01-05 12:53:45 +0800

    main

--- project/src/components/main.js
+++ project/src/components/main.js
@@ -6,15 +6,16 @@ import CategoryContainer from './CategoryContainer'
 import  {Route} from 'react-router-dom'
 import MainDish from './MainDish'
 import DesertComponent from './DesertComponent'
+import { Container } from 'react-bootstrap'
 function Main() {
   return (
     <>
-    <Submenu/>
+    <Container> <Submenu/>
       <Route exact path='/main' component={MainDish}></Route>
       <Route path='/main/discountProduct' component={DiscountProduct}></Route>
       <Route path='/main/saladAndDesert' component={SaladAndDesert}></Route>
       <Route path='/main/categoryContainer' component={CategoryContainer}></Route>
-      <Route path='/main/desert' component={DesertComponent}></Route>
+      <Route path='/main/desert' component={DesertComponent}></Route></Container>
     </>
   )}
 export default Main
