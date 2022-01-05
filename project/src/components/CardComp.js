7cb3d17f78cd9798d309f9f66e1ef4cb5688cc4c
lab_1_19 <mstarsacademy@MStarss-iMac.local>
2022-01-05 16:34:12 +0800

    #53 edited price calculated final price

--- project/src/components/CardComp.js
+++ project/src/components/CardComp.js
@@ -14,11 +14,11 @@ const CardComp = (props) => {
                   <Card.Img variant="top" src={foodsDetail.thumb_img} />
                   <Card.Title className="mt-3 mx-2 foodTitle">{foodsDetail.name}</Card.Title>
                   <div className="prices mx-2 row ">
-                    <Card.Text className="col-5 priceTag" style={{ color: '#f17228' }}>
-                      {foodsDetail.final_price}
+                    <Card.Text className="col-5 priceTag mx-2" style={{ color: '#f17228' }}>
+                      {foodsDetail.final_price = foodsDetail.price - (foodsDetail.price * foodsDetail.discount_percentage / 100)}₮
                     </Card.Text>
                     <Card.Text className="finalPrice col-5">
-                      <strike>{foodsDetail.price}</strike>
+                      <strike style={foodsDetail.discount_percentage== 0 ? {display: 'none'}:{display: 'block'}}>{foodsDetail.price}</strike>
                     </Card.Text>
                     <div className="col-2"></div>
                   </div>
@@ -27,7 +27,7 @@ const CardComp = (props) => {
                   <Card.Text style={{ display: 'none' }}>{foodsDetail.stock}</Card.Text>
                   <Card.Text style={{ display: 'none' }}>{foodsDetail.recipe}</Card.Text>
                   <Card.Text style={{ display: 'none' }}>{foodsDetail.category}</Card.Text>
-                  <Card.Text className='discount_percentage'>{foodsDetail.discount_percentage}</Card.Text>
+                  <Card.Text className='discount_percentage' style={foodsDetail.discount_percentage== 0 ? {display: 'none'}:{display: 'block'}}>{foodsDetail.discount_percentage}%</Card.Text>
                 </Card.Body>
               </Card>
             </div>
