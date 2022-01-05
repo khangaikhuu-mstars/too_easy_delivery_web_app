c6942bfd4dcfd1dcf7c12353957434f2d009c037
lab_1_19 <mstarsacademy@MStarss-iMac.local>
2022-01-05 10:47:18 +0800

    #33 created discount component

--- project/src/components/CategoryContainer.js
+++ project/src/components/CategoryContainer.js
@@ -1,10 +1,10 @@
 import MainDish from "./MainDish";
 import SaladAndDesert from "./SaladAndDesert";
-
+import DiscountProduct from './DiscountProduct'
 function CategoryContainer() {
     return(
         <div className="container">
-              <div className="mainDish ">
+              <div className="category ">
                 <h2 className="title"> Үндсэн хоол</h2>
                     <button className="seeMoreButton" type="button">
                         <h2 className="onWeb">Бүгдийг харах</h2> 
@@ -13,7 +13,7 @@ function CategoryContainer() {
              </div>
                     <MainDish/>
 
-             <div className="SaladAndDesert ">
+             <div className="category">
                 <h2 className="title"> Салат ба зууш</h2>
                     <button className="seeMoreButton" type="button">
                         <h2 className="onWeb">Бүгдийг харах</h2> 
@@ -22,13 +22,14 @@ function CategoryContainer() {
                </div>
                     <SaladAndDesert/>
 
-             <div className="Discount">
+             <div className="category">
                 <h2 className="title">Хямдралтай</h2>
                     <button className="seeMoreButton" type="button">
                         <h2 className="onWeb">Бүгдийг харах</h2> 
                         <span className="rightArrow">&#62;</span>
                     </button>
                </div>
+                    <DiscountProduct/>
         </div>
     )
 }
