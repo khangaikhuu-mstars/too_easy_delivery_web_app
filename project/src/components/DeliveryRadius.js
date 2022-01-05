b87b708fae157ba2c3153fde302dbb402a4c10f7
lab_1_01 <lab_1_01@lab-1-01s-iMac.local>
2022-01-05 16:15:28 +0800

    #53 issue

--- project/src/components/DeliveryRadius.js
+++ project/src/components/DeliveryRadius.js
@@ -1,11 +1,8 @@
 import React from 'react-bootstrap'
 import Icon from '../images/MobileLocationIcon.svg'
 
-
 function DeliveryRadius() {
-
     return (
-
         <div>
             <div className='TabMap'>
                 <div className='container'>
@@ -15,17 +12,14 @@ function DeliveryRadius() {
                     </div>
                 </div>
             </div>
-
             <div className='container'>
                 <div className='TextBus'>
                     <div className='DeliveryText'>
                         Хүргэлтийн бүс
                     </div>
                 </div>
-
             </div>
             <div className='container MobileBus'>
-
                 <div className='TabText'>
                     <div className='TextWidth'>
                         <div >
@@ -35,12 +29,11 @@ function DeliveryRadius() {
                                         "A" бүс
                                     </div>
                                 </div>
-
                                 <div className='BusText'>
 
-                                    <div className='BusList'> <img src={Icon} className=' d-md-none'/>  Нархан хотгон</div>
-                                    <div className='BusList'> <img src={Icon} className=' d-md-none'/>  26-байр</div>
-                                    <div className='BusList'> <img src={Icon} className=' d-md-none'/>  Хоймор хотгон</div>
+                                    <div className='BusList'> <img src={Icon} className=' d-md-none' />  Нархан хотгон</div>
+                                    <div className='BusList'> <img src={Icon} className=' d-md-none' />  26-байр</div>
+                                    <div className='BusList'> <img src={Icon} className=' d-md-none' />  Хоймор хотгон</div>
                                     <div className='BusList2'> 56-байр</div>
                                     <div className='BusList2'> 36-байр</div>
                                     <div className='BusList2'> 16-байр</div>
@@ -48,9 +41,9 @@ function DeliveryRadius() {
                                     <div className='BusList2'> 46-байр</div>
                                 </div>
                                 <div className='BusText'>
-                                    <div className='BusList'> <img src={Icon} className=' d-md-none'/>  Нархан хотгон</div>
-                                    <div className='BusList'> <img src={Icon} className=' d-md-none'/>  26-байр</div>
-                                    <div className='BusList'> <img src={Icon} className=' d-md-none'/>  Хоймор хотгон</div>
+                                    <div className='BusList'> <img src={Icon} className=' d-md-none' />  Нархан хотгон</div>
+                                    <div className='BusList'> <img src={Icon} className=' d-md-none' />  26-байр</div>
+                                    <div className='BusList'> <img src={Icon} className=' d-md-none' />  Хоймор хотгон</div>
                                     <div className='BusList2'> 56-байр</div>
                                     <div className='BusList2'> 36-байр</div>
                                     <div className='BusList2'> 16-байр</div>
@@ -69,11 +62,10 @@ function DeliveryRadius() {
                                         "Б" бүс
                                     </div>
                                 </div>
-
                                 <div className='BusText'>
-                                    <div className='BusList'> <img src={Icon} className=' d-md-none'/> Нархан хотгон</div>
-                                    <div className='BusList'> <img src={Icon} className='d-md-none'/> 26-байр</div>
-                                    <div className='BusList'> <img src={Icon} className=' d-md-none'/> Хоймор хотгон</div>
+                                    <div className='BusList'> <img src={Icon} className=' d-md-none' /> Нархан хотгон</div>
+                                    <div className='BusList'> <img src={Icon} className='d-md-none' /> 26-байр</div>
+                                    <div className='BusList'> <img src={Icon} className=' d-md-none' /> Хоймор хотгон</div>
                                     <div className='BusList2'> 56-байр</div>
                                     <div className='BusList2'> 36-байр</div>
                                     <div className='BusList2'> 16-байр</div>
@@ -81,9 +73,9 @@ function DeliveryRadius() {
                                     <div className='BusList2'> 46-байр</div>
                                 </div>
                                 <div className='BusText'>
-                                    <div className='BusList'> <img src={Icon} className=' d-md-none'/> Нархан хотгон</div>
-                                    <div className='BusList'> <img src={Icon} className=' d-md-none'/> 26-байр</div>
-                                    <div className='BusList'> <img src={Icon} className=' d-md-none'/>Хоймор хотгон</div>
+                                    <div className='BusList'> <img src={Icon} className=' d-md-none' /> Нархан хотгон</div>
+                                    <div className='BusList'> <img src={Icon} className=' d-md-none' /> 26-байр</div>
+                                    <div className='BusList'> <img src={Icon} className=' d-md-none' />Хоймор хотгон</div>
                                     <div className='BusList2'> 56-байр</div>
                                     <div className='BusList2'> 36-байр</div>
                                     <div className='BusList2'> 16-байр</div>
@@ -91,23 +83,17 @@ function DeliveryRadius() {
                                     <div className='BusList2'> 46-байр</div>
                                 </div>
                             </div>
-
                         </div>
                     </div>
-
                 </div>
-
-
             </div>
             <div className='TabMap2'>
-
             </div>
-
-
         </div>
 
     )
 }
+
 export default DeliveryRadius
 
 
