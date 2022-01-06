ba3f2e413ddd5d3a5900824ffd60bf57975ac535
lab_1_01 <lab_1_01@lab-1-01s-iMac.local>
2022-01-06 12:38:12 +0800

    #61 cardModule

--- project/src/components/CardComp.js
+++ project/src/components/CardComp.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import { Card} from 'react-bootstrap'
-import '../css/cardComp.css';
+import { React, useState } from 'react'
+import { Card } from 'react-bootstrap'
+import '../css/cardComp.css'
+import CardModal from './CardModal'
 
 const CardComp = (props) => {
+  const [modalShow, setModal] = useState(false)
   return (
     <div className="mainCard flex-container">
       {props.data.map((foodsDetail) => {
         console.log(foodsDetail)
         return (
-            <div className="row">
-              <Card className={foodsDetail.sales ? "cardComponentWithBadge":"cardComponentWithoutBadge" } style={{ width: '16rem' }}>
+          <div className="row">
+            <div onclick = {()=> setModal(true)}>
+              <Card 
+                className={foodsDetail.sales ? 'cardComponentWithBadge' : 'cardComponentWithoutBadge'}
+                style={{ width: '16rem' }} 
+              >
                 <Card.Body>
                   <Card.Img variant="top" src={foodsDetail.thumb_img} />
                   <Card.Title className="mt-3 mx-2 foodTitle">{foodsDetail.name}</Card.Title>
@@ -27,10 +33,12 @@ const CardComp = (props) => {
                   <Card.Text style={{ display: 'none' }}>{foodsDetail.stock}</Card.Text>
                   <Card.Text style={{ display: 'none' }}>{foodsDetail.recipe}</Card.Text>
                   <Card.Text style={{ display: 'none' }}>{foodsDetail.category}</Card.Text>
-                  <Card.Text className='discount_percentage'>{foodsDetail.discount_percentage}</Card.Text>
+                  <Card.Text className="discount_percentage">{foodsDetail.discount_percentage}</Card.Text>
                 </Card.Body>
               </Card>
+              {modalShow && <CardModal />}
             </div>
+          </div>
         )
       })}
     </div>
