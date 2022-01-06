b193b2c7775f09b289df21dad2546702f91120cb
lab_1_1x <lab_1_15@lab-1-15s-iMac.local>
2022-01-06 11:27:38 +0800

    renamed components

--- project/src/App.js
+++ project/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Footer from './components/Footer';
-import Menu from './components/menu';
+import Menu from './components/Menu';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Delivery from './components/delivery'
-import Main from './components/main'
-import Home from './components/home'
+import Delivery from './components/Delivery'
+import Main from './components/Main'
+import Home from './components/Home'
 import './css/footer.css'
 import './App.css'
 import '../src/css/InfoPanel.css'
