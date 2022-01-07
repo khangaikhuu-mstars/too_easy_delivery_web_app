bcb5c5fb74667b1736d76b03664252cb6e4461a9
khangaikhuu uvgunkhuu <khangaikhuu@mstars.mn>
2022-01-07 15:02:42 +0800

    #69 fixing navlink component from react-route-dom'

--- project/src/components/MenuComponent.js
+++ project/src/components/MenuComponent.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar, Container, Nav, Form, FormControl } from 'react-bootstrap'
+import { Navbar, Container, Nav, Form, FormControl, NavLink } from 'react-bootstrap'
 import foodDeliveryLogo from '../image/fooddelivery.svg'
 import adminLogo from '../image/admin.svg'
 import basket from '../image/basketIcon.svg'
