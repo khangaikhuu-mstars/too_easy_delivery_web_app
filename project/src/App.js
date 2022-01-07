2cf392f19ccfd01767a1d4189db98bc43f23381f
khangaikhuu uvgunkhuu <khangaikhuu@mstars.mn>
2022-01-07 14:57:54 +0800

    #69 remove head and other conflicts

--- project/src/App.js
+++ project/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Delivery from './components/DeliveryComponent'
 import Main from './components/MainComponent'
 import Home from './components/HomeComponent'
+import { Redirect } from 'react-router-dom'
 import './css/footer.css'
 import './App.css'
 import '../src/css/InfoPanel.css'
@@ -12,11 +13,7 @@ import '../src/css/sliderWeb.css'
 import '../src/css/menu.css'
 import '../src/css/submenu.css'
 import '../src/css/categoryContainer.css'
-<<<<<<< HEAD
 import '../src/css/search.css'
-=======
-import { Redirect } from 'react-router-dom'
->>>>>>> cb64005eacfd3f3122ad79110000643e50faa37f
 
 function App() {
   return (
