8790b2b0bd30c5bf61008ed9583a70bc2383054a
Temka1358 <temulenn@gmail.com>
2022-01-14 01:03:01 +0800

    removed mt-3 from breadcrumb

--- project/src/components/BreadCrumbComponent.js
+++ project/src/components/BreadCrumbComponent.js
@@ -4,7 +4,7 @@ import Breadcrumb from 'react-bootstrap/Breadcrumb'
 const BreadCrumbComponent = (props) => {
   return (
     <div>
-      <Breadcrumb mt-3>
+      <Breadcrumb>
         <Breadcrumb.Item >Хайлт</Breadcrumb.Item>
         <Breadcrumb.Item active>{props.data}</Breadcrumb.Item>
       </Breadcrumb>
