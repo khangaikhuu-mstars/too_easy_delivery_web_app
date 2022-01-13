f4fdb00eb4f5b5914a76186a523d005981c4abde
Temka1358 <temulenn@gmail.com>
2022-01-14 00:49:40 +0800

    #69 [ADD] breadcrumb design for search result

--- project/src/components/BreadCrumbComponent.js
+++ project/src/components/BreadCrumbComponent.js
@@ -4,8 +4,8 @@ import Breadcrumb from 'react-bootstrap/Breadcrumb'
 const BreadCrumbComponent = (props) => {
   return (
     <div>
-      <Breadcrumb>
-        <Breadcrumb.Item active>Хайлт</Breadcrumb.Item>
+      <Breadcrumb mt-3>
+        <Breadcrumb.Item >Хайлт</Breadcrumb.Item>
         <Breadcrumb.Item active>{props.data}</Breadcrumb.Item>
       </Breadcrumb>
     </div>
